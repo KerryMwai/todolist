985a0968ae808320f61d3445d07bc8fd43d396cf
Kerry Mwai <kerrywangamati@gmail.com>
2021-03-05 21:49:19 +0300

    todolist

---
+++ todo.js
@@ -0,0 +1,60 @@
+$("document").ready(function () {
+    $("input[type=checkbox]").removeAttr("checked");
+    $("#learningInsititutions").tabs();
+    $("ul").sortable({axis:"x", containment:"#learningInsititutions"})
+    $("ol").sortable({ axis: "y", containment: "#learningInsititutions" })
+
+    //to remove or delete the content with the checkbox
+    $("#learningInsititutions").on("click","input[type=checkbox]",function () {
+        $(this).closest("li").slideUp(function () {
+            $(this).remove();
+        })
+    })
+    //add project button
+    $("#btnAddSchoolLevel").button().click(function () {
+        $("#level-dialog").dialog({
+            width: 400, resizable: false, modal: true,
+            buttons: {
+                //add button of project
+                "Add new Level": function () {
+                    var projectName = $("#new-level").val();
+                    var replaceSpaces=projectName.split(" ").join("_")
+                    $("<li><a href='#" + replaceSpaces + "'>" + projectName + "</a></li>").appendTo("#main");
+                    $("<ol id='" + replaceSpaces + "'></ol>").appendTo("#learningInsititutions")
+                    $("#learningInsititutions").tabs("refresh");
+                    var tabCount = $("#learningInsititutions .ui-tabs-nv li").length;
+                    $("#learningInsititutions").tabs("option", "active", tabCount - 1);
+                    $("#new-level").val("");
+                    $(this).dialog("close");
+                },
+                //cancel button of project
+                "Cancel": function () {
+                    $("#new-level").val("");
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
+
+    //add task button
+    $("#btnAddSchool").button().click(function () {
+        $("#school-dialog").dialog({
+            width: 400, resizable: false, modal: true,
+            buttons: {
+                "Add a School": function () { 
+                    $("#learningInsititutions").tabs("refresh");
+                    var activeTab = $("#learningInsititutions").tabs("option", "active");
+                    var title = $("#main>li:nth-child(" + (activeTab + 1) + ")>a").attr("href");
+                    var popup = $("#learningInsititutions " + title).append("<li><input type='checkbox'>" + $("#new-school").val() + "</li>").sortable();
+                    $("#new-school").val("");
+                    $(this).dialog("close");
+                },
+                "cancel": function () {
+                    $("#new-school").val("");
+                    $(this).dialog("close");
+                }
+            }
+        }
+        )
+    })
+})
